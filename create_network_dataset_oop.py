13106c6ad63bb41085d437f323ca3ee1a3961a50
aaron <aaronrumph0@gmail.com>
2025-10-17 04:19:58 -0500

    v0.6.0.5 wasted time fixing counter for elevation mapper and adding a timer (much nicer knowing roughly when it will finish running)

--- create_network_dataset_oop.py
+++ create_network_dataset_oop.py
@@ -40,7 +40,41 @@ arc_project_path = config.arc_project_path
 arc_project_file = arcpy.mp.ArcGISProject(os.path.join(arc_project_path, r"network_dataset_package_project.aprx"))
 arcgis_py_env = config.arcgis_py_env
 
+def init_worker(counter, lock):
+    # initializer function for multiprocess processes, need in order to have a global variable accessible to all workers
+    global shared_counter, shared_lock
+    shared_counter = counter
+    shared_lock = lock
 
+def generic_get_elevation_function(start_time, total_nodes, idx, x_y: tuple):
+    # this is the generic function for getting elevation from API for a point x_Y
+    usgs_url = r"https://epqs.nationalmap.gov/v1/json"
+    lon = x_y[0]
+    lat = x_y[1]
+    parameters_for_usgs_api = {"x": lon, "y": lat, "units": "Meters", "wkid": 4326}
+
+    try:
+        response = requests.get(usgs_url, params=parameters_for_usgs_api, timeout=10)
+        response.raise_for_status()
+        data = response.json()
+        elevation = data.get('value')
+
+        if elevation == -1000000:  # no data available (-1000000 is default for no value)
+            elevation = None
+
+        # Fixed node counter
+        with shared_lock:
+            shared_counter.value += 1
+            current = shared_counter.value
+        time_since_start = time.perf_counter() - start_time
+        print(f"\rElevation: {current}/{total_nodes} nodes | Time: {time_since_start:.1f}s / "
+              f"{(total_nodes / 7.8):.1f}s", end="", flush=True)
+
+        return elevation
+
+    except Exception as e:
+        logging.warning(f"Error getting elevation for node {idx}: {e}")
+        return None
 
 class CacheFolder:
     def __init__(self, network_snake_name):
@@ -70,8 +104,7 @@ class CacheFolder:
             )
         else:
             os.makedirs(self.path, exist_ok=True)
-            
-    
+
 # simple Cache class with obvious methods (read, write, check if exists)
 class Cache:
     def __init__(self, cache_folder:CacheFolder, cache_name):
@@ -87,7 +120,7 @@ class Cache:
 
     def read_cache_data(self):
         if self.check_if_cache_already_exists():
-            with open(self.cache_path, "wb") as cache_file:
+            with open(self.cache_path, "rb") as cache_file:
                 cache_data = pickle.load(cache_file)
             return cache_data
         else:
@@ -96,8 +129,6 @@ class Cache:
     def write_cache_data(self, data_to_cache):
         with open(self.cache_path, "wb") as cache_file:
             pickle.dump(data_to_cache, cache_file)
-
-
 
 # StreetNetwork class very bare bones, just gets street network graph and makes associated GeoDataframse
 class StreetNetwork:
@@ -131,10 +162,11 @@ class StreetNetwork:
     def get_street_network_from_osm(self, timer_on=True):
         # this method does the brunt of the work for the class
         start_time = time.perf_counter()
-        logging.info("Getting street network from OSM")
+        logging.info("Getting street network")
 
         # first if not using cache or no cache data
         if (not self.use_cache) or (not self.graph_cache.check_if_cache_already_exists()):
+            logging.info("Getting street network from OSM")
 
             # if using city, getting OSM data if not using cache or if no cache exists
             if self.bound_box is None:
@@ -148,6 +180,7 @@ class StreetNetwork:
 
         # otherwise using cached data
         else:
+            logging.info("Using cached street network")
             network_graph = self.graph_cache.read_cache_data()
             network_nodes, network_edges = self.nodes_cache.read_cache_data(), self.edges_cache.read_cache_data()
 
@@ -159,55 +192,26 @@ class StreetNetwork:
 
         return network_graph, network_nodes, network_edges
 
-
-
 # ElevationMapper class adds elevation to nodes and grades to edges (coming soon?) using USGS EPQS API
 class ElevationMapper:
     # WARNINGS!: 1. Adding elevation takes forever, ~130ms per node with 14 threads (1.8s per node per thread!) 
     #                   because the API is so slow
     #            2. Uses multiprocessing (scary) so DO NOT take out the "if __name__ == ...." guard!!!!!
 
-    def __init__(self, street_network:StreetNetwork, threads_available=mp.cpu_count()):
+    def __init__(self, street_network:StreetNetwork, threads_available=mp.cpu_count(), reset=False):
         self.street_network = street_network
         self.threads_available = threads_available
+        self.reset = reset
         (self.street_network_graph, self.street_network_nodes, self.street_network_edges) = (
             self.street_network.get_street_network_from_osm()
         )
         self.node_counter = 0
         self.elevation_cache = Cache(street_network.cache_folder, "elevation")
 
-    
-    def generic_get_elevation_function(self, total_nodes, idx, x_y:tuple):
-        # this is the generic function for getting elevation from API for a point x_Y
-        usgs_url = r"https://epqs.nationalmap.gov/v1/json"
-        lon = x_y[0]
-        lat = x_y[1]
-        parameters_for_usgs_api = {"x": lon, "y": lat, "units": "Meters", "wkid": 4326}
-
-        try:
-            response = requests.get(usgs_url, params=parameters_for_usgs_api, timeout=10)
-            response.raise_for_status()
-            data = response.json()
-            elevation = data.get('value')
-
-            if elevation == -1000000:  # no data available (-1000000 is default for no value)
-                elevation = None
-
-            ### NEED TO FIX NODE COUNTER: need to make so counts total rather than for each thread (I think?)
-            self.node_counter += 1 # adds one to the count of how many nodes have been processed
-            print(f"\rElevation added for {self.node_counter}/{total_nodes} nodes", end='', flush=True)
-
-            return elevation
-
-        except Exception as e:
-            logging.warning(f"Error getting elevation for node {idx}: {e}")
-            return None
-
     # using multiprocessing to get elevation data if no cache exists
     def add_elevation_data_to_nodes(self):
         # in case no elevation data exists
-        if not self.elevation_cache.check_if_cache_already_exists():
-            start_time = time.perf_counter()
+        if not self.elevation_cache.check_if_cache_already_exists() or self.reset:
             logging.info("Getting elevation data from USGS EPQS API")
     
             number_of_nodes_total = len(self.street_network_nodes)
@@ -220,20 +224,27 @@ class ElevationMapper:
                 x_y_pair = (row["x"], row["y"])
                 x_y_args_for_elevation.append(x_y_pair)
 
+            shared_counter = mp.Value("i", 0)  # 'i' for integer
+            lock = mp.Lock()
+            start_time = time.perf_counter()
             # using multiprocessing to send as many API requests at once as possible
-            with mp.Pool(processes=self.threads_available) as pooled_threads:
-                z_values = pooled_threads.starmap(self.generic_get_elevation_function,
-                    zip(repeat(number_of_nodes_total), idx_args_for_elevation, x_y_args_for_elevation)
+            with mp.Pool(processes=self.threads_available,
+            initializer=init_worker,
+            initargs=(shared_counter, lock)) as pooled_threads:
+                z_values = pooled_threads.starmap(generic_get_elevation_function,
+                    zip(repeat(start_time), repeat(number_of_nodes_total),
+                        idx_args_for_elevation, x_y_args_for_elevation
+                    )
                 )
             pooled_threads.close()
 
             self.street_network_nodes["z"] = z_values
             self.elevation_cache.write_cache_data(self.street_network_nodes["z"].to_dict())
 
+            # outputs for method
             run_time = time.perf_counter() - start_time
             logging.info(f"Got elevation data for {len(self.street_network_nodes)} nodes in {run_time} seconds")
             return self.street_network_nodes
-
         else:
             z_values = self.elevation_cache.read_cache_data()   # loading elevation data from cache
 
@@ -245,6 +256,10 @@ class ElevationMapper:
                 logging.info("Nodes gdf already has z values")
 
             return self.street_network_nodes
+
+    # need to calculate grades for edges so can make walking time vary with slope
+    def add_grades_to_edges(self):
+        pass
 
 
 class GeoDatabase:
@@ -263,8 +278,8 @@ class NetworkDataset:
 # # # # # # # # # # # # # # # # # # Testing Area :::: DO NOT REMOVE "if __name__ ..." # # # # # # # # # # # # # # # # #
 
 if __name__ == "__main__":
-    pinole_street_network = StreetNetwork("Pinole, California, USA")
-    pinole_street_network_with_elevation = ElevationMapper(pinole_street_network)
-    print(pinole_street_network_with_elevation.add_elevation_data_to_nodes())
+    pinole_street_network = StreetNetwork("El Sobrante, California, USA")
+    pinole_street_network_with_elevation = ElevationMapper(pinole_street_network, reset=True)
+    print(pinole_street_network_with_elevation.add_elevation_data_to_nodes().head())
 
 
